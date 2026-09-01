0d17b4aa3a2d653c75f4dc3c1ba286fdfdb1c1f1
rmassoud-coder <rmassoud@gmail.com>
2026-09-01 17:33:35 +0300

    Update MassPowerLogo.tsx

--- frontend/src/components/MassPowerLogo.tsx
+++ frontend/src/components/MassPowerLogo.tsx
@@ -63,7 +63,7 @@ const MassPowerLogo = ({ width = 75, height = 75 }) => {
       </G>
 
       <SvgText
-        x="400"
+        x="420"
         y="680"
         fontSize="180"
         fontWeight="bold"
